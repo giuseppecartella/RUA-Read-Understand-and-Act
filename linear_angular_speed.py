9918b408e93338c8c657cee70c54afeaf70b90a9
Nicholas Moratelli <48736545+NicholasMoratelli@users.noreply.github.com>
2021-12-06 18:20:33 +0100

    Update linear_angular_speed.py

--- linear_angular_speed.py
+++ linear_angular_speed.py
@@ -1,116 +1,109 @@
 import numpy as np
 from pyrobot import Robot
 from pyrobot.locobot.base_control_utils import (
-    get_trajectory_circle,
-    get_trajectory_negcircle,
     get_control_trajectory, 
     get_state_trajectory_from_controls
 )
 
-def get_trajectory_rotate_dx(start_pos, dt, r, v, angle):
-    #dt = s /v
-    #dt = 1.0 / dt
+def get_trajectory_rotate_sx(start_pos, dt, r, v, angle):
     w = v / r
     T = int(np.round(angle / w / dt))
     controls = get_control_trajectory("rotate", T, v, w)
     states = get_state_trajectory_from_controls(start_pos, dt, controls)
     return states, controls
 
-def get_trajectory_rotate_sx(start_pos, dt,  r, v, angle):
-    #dt = s /v
-    #dt = 1.0 / dt
+def get_trajectory_rotate_dx(start_pos, dt, r, v, angle):
     w = v / r
     T = int(np.round(angle / w / dt))
     controls = get_control_trajectory("negrotate", T, v, w)
     states = get_state_trajectory_from_controls(start_pos, dt, controls)
     return states, controls
 
-def get_trajectory_straight(start_pos, dt, r, v, angle):
-    #dt = s /v 
-    #dt = 1.0 / dt
+def get_trajectory_straight(start_pos, dt, r, v, s):
     w = v / r
-    T = int(np.round(angle / w / dt))
+    T = int(np.round(s / v / dt)) 
+
+    print("ENtrato")
     
     controls = get_control_trajectory("straight", T, v, w)
     states = get_state_trajectory_from_controls(start_pos, dt, controls)
     return states, controls
 
 
-def get_trajectory(bot):
+def get_trajectory(bot, path):
     v = bot.configs.BASE.MAX_ABS_FWD_SPEED
     w = bot.configs.BASE.MAX_ABS_TURN_SPEED
 
-    s = 1.0 #1 meter ahead
-    # v = s / t
-    # v t = s
-    # t = s / v
-    dt = 1.0 / (s / v)
+    s = 3.0
+    dt = 1. / (s / v)
     r = v / w
+
     start_state = np.array(bot.base.get_state("odom"))
-    states1, _ = get_trajectory_straight(start_state,dt , r, v, np.pi)
-    print(start_state.shape, states1.shape)
-    final_states = np.concatenate([start_state, states1], 0)
+    print(start_state)
+    concatenate_state = []
+    concatenate_state.append(start_state)
 
-    return final_states
+    for i, p in enumerate(path):
+        print(i)
+        if i == 0:
+            val = start_state
 
+            if abs(p[1]) < 0.05:
+                state1, _ = get_trajectory_straight(val, dt, r, v, p[0])
+                first_rotation = False
+            else:
+                if p[1] < 0:
+                    state1, _ = get_trajectory_rotate_dx(val, dt, r, v, abs(p[2]))
+                else:
+                    state1, _ = get_trajectory_rotate_sx(val, dt, r, v, abs(p[2]))
 
-    """
-    s = 2.0
-    dt = 1.0 / bot.configs.BASE.BASE_CONTROL_RATE
+                state2, _ = get_trajectory_straight(concatenate_state[-1, :].copy(), dt, r, v, p[0])
+                concatenate_state = np.concatenate([state1, state2], 0)
+                first_rotation = True
+        else:
+            if not first_rotation:
+                val = state1[-1, :].copy()
+                first_rotation = True
+            else:
+                val = concatenate_state[-1, :].copy()
+            
+            print("VAL --> ", val)
+            if abs(p[1]) < 0.05:
+                state, _ = get_trajectory_straight(val, dt, r, v, p[0])
+            else:
+                if p[1] < 0:
+                    state, _ = get_trajectory_rotate_dx(val, dt, r, v, abs(np.arctan2(p[1],p[0]+0.000001)) )
+                else:
+                    state, _ = get_trajectory_rotate_sx(val, dt, r, v, abs(np.arctan(p[1],p[0]+0.000001)) )
+
+                print(state1, state)
+                print(state1.shape, state.shape)
+                if i > 1:
+                    concatenate_state = np.concatenate([concatenate_state, state], 0)
+                else:
+                    concatenate_state = np.concatenate([state1, state], 0)
+                print(concatenate_state.shape, concatenate_state)
+                state, _ = get_trajectory_straight(concatenate_state[-1, :].copy(), dt, r, v, p[0])
+        
+            concatenate_state = np.concatenate([concatenate_state, state], 0)
+        
+    '''states1, _ = get_trajectory_straight(start_state, dt, r, v, 1.0)
+    states2, _ = get_trajectory_rotate_dx(states1[-1, :].copy(), dt, r, v, np.pi/2)
+    states = np.concatenate([states1, states2], 0)
+    state3, _ = get_trajectory_straight(states[-1, :].copy(), dt, r, v, 0.20)
+    states = np.concatenate([states, state3], 0)
+    states4, _ = get_trajectory_rotate_sx(states[-1, :].copy(), dt, r, v, np.pi/2)
+    states = np.concatenate([states, states4], 0)
+    state5, _ = get_trajectory_straight(states[-1, :].copy(), dt, r, v, 1.0)
+    states = np.concatenate([states, state5], 0)   '''
+
+    return concatenate_state
+
     
-    print("dt :", dt)
-    #v = 2.0 / dt
-    v = bot.configs.BASE.MAX_ABS_FWD_SPEED
-    dt = (v / s)
-    w = bot.configs.BASE.MAX_ABS_TURN_SPEED
-    r = v / w
-    print("R: " ,r)
-    print("v: " , v)
-    print("w: ", w)
-    print("inizio traiettoria")
-    start_state = np.array(bot.base.get_state("odom"))
-    states1, _ = get_trajectory_straight(start_state,dt , r, v, np.pi)
-
-    states_new, _ = get_trajectory_straight(states1[-1,:].copy(),dt , r, v, np.pi)
-    new_concat = np.concatenate([states1, states_new], 0)
-    states2, _ = get_trajectory_rotate_dx(new_concat[-1, :].copy(), dt, r, v, np.pi/2)
-    
-    states2_new = np.concatenate([new_concat, states2], 0)
-    s = 1.0
-    dt = (v / s)
-    #v = 1.0 / dt
-    r = v / w
-    print("R: " ,r)
-    print("v: " , v)
-    print("w: ", w)
-       
-
-    states3, _ = get_trajectory_straight(states2[-1, :].copy(), dt , r, v , np.pi)
-    states2_new = np.concatenate([states2_new, states3], 0)
-
-    #states = np.concatenate([states1, states2], 0)
-    states3 = np.concatenate([states2, states3], 0)
-
-    states4, _ = get_trajectory_rotate_sx(states3[-1, :].copy(), dt, r, v, np.pi/2)
-
-    states4 = np.concatenate([states3, states4], 0)
-    #v = 5.0 / dt
-    r = v/w
-    print("R: " ,r)
-    print("v: " , v)
-    print("w: ", w)
-
-    states5, _ = get_trajectory_straight(states4[-1, :].copy(), dt , r, v , np.pi)
-
-    
-
-    states = np.concatenate([states4, states5], 0)
-    print(states)
-    #return states
-    return states2_new
-    """
-
+path = [[1.24, -0.01, -0.008064341306767012], [0.26, -0.29, -0.8398896196381794]]
+path = [[0.74, -0.01, -0.013512691013328216], [0.26, -0.75, -1.2370941502573463], [0.26, -0.01, -0.03844259002118798], [0.24, -0.0, 0.0]]
 print("sono entrato")
 bot = Robot('locobot')
-states = get_trajectory(bot)
+bot.camera.reset()
+states = get_trajectory(bot, path)
 bot.base.track_trajectory(states, close_loop=True)
